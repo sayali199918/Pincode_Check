6aa5aa1a17050676679a93fb1b6ccf95bdb8cdeb
sayali199918 <sayaliprashantbadgujar@gmail.com>
2020-07-14 04:48:41 -0700

    Added:Added a Space after thethree digit pincode

--- pincodde.sh
+++ pincodde.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -x
 echo "enter the pincode:" pincode
 read pincode
-pinpat="^[0-9]{6}$"
+pinpat="^[0-9]{6}$|[0-9]{3}[ :space: ][0-9]{3}$"
 if [[ $pincode =~ $pinpat ]]
 then
 	echo "valid pincode"
